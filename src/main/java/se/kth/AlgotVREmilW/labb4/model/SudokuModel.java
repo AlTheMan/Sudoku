24f8d71d51e4dad37c62a41a7da3361289529148
AlTheMan <algot98@hotmail.com>
2022-10-16 21:57:28 +0200

    Javadoc i model

--- src/main/java/se/kth/AlgotVREmilW/labb4/model/SudokuModel.java
+++ src/main/java/se/kth/AlgotVREmilW/labb4/model/SudokuModel.java
@@ -20,7 +20,10 @@ import static se.kth.AlgotVREmilW.labb4.model.SudokuUtilities.*;
     }
 
 
-
+        /**
+         * Creates a game instance from file. Is used when user has saved a game on file
+         * and wants to reload
+         */
     private void updateFacadeGameState(){
         int[][] gameState = new int[GRID_SIZE][GRID_SIZE];
         for (int i = 0; i<9; i++){
@@ -31,6 +34,10 @@ import static se.kth.AlgotVREmilW.labb4.model.SudokuUtilities.*;
         facade.gameState = gameState;
     }
 
+        /**
+         * Creates a new game with input difficulty
+         * @param difficulty specified difficulty
+         */
     public void changeDifficulty(SudokuLevel difficulty) {
         this.game = generateSudokuMatrix(difficulty);
         randomizeGameBoard();
@@ -39,25 +46,48 @@ import static se.kth.AlgotVREmilW.labb4.model.SudokuUtilities.*;
         updateFacadeGameState();
     }
 
+        /**
+         * Loads game from input array. Is used to load a game from
+         * previously saved file
+         * @param loadedGame input array that stores the full game-state.
+         */
     public void loadGame(int[][][] loadedGame) {
         this.game = loadedGame;
         createSudokuTiles();
         updateFacadeGameState();
     }
 
+        /**
+         *
+         * @return array of the game-state (not a copy)
+         */
     public int[][][] getGame(){
         return game;
     }
 
+        /**
+         * returns a copy of game at initial positions (before a user has inputted any numbers)
+         * @return array of the game-state at starting positions.
+         */
     public int[][] getGameAtStartCopy() {
         return gameAtStartCopy;
     }
 
+        /**
+         * Updates the initial game-states starting positions (before users
+         * have inputed any numbers). I.e updates the starting-numbers.
+         * is used when loading game from file.
+         * @param gameAtStartCopy the array used to keep track of starting-numbers.
+         */
     public void setGameAtStartCopy(int[][] gameAtStartCopy) {
         this.gameAtStartCopy = gameAtStartCopy;
     }
 
 
+        /**
+         * Creates a 2d array of SudokuTile:
+         * SudokuTile[][]
+         */
     public void createSudokuTiles() {
         for (int i = 0; i < GRID_SIZE; i++) {
             for (int j = 0; j < GRID_SIZE; j++) {
@@ -78,26 +108,62 @@ import static se.kth.AlgotVREmilW.labb4.model.SudokuUtilities.*;
     }
 
 
+        /**
+         * Returns a 3d array with either the solution or current state of the game
+         * depending on which depth specified.
+         * @param row specified row
+         * @param col specified column
+         * @param depth 0 indicates user-inputed numbers and initial starting-numbers. 1 indicates the array used for the solution
+         * @return
+         */
     public int getSudokuMatrix(int row, int col, int depth) {
         return game[row][col][depth];
     }
 
+        /**
+         * returns an array of current state of the game
+         * @return
+         */
     public SudokuTile[][] getSudokuTiles(){
         return sudokuTiles;
     }
 
+        /**
+         * Returns the value at specified row and column
+         * @param x specified row
+         * @param y specified column
+         * @return
+         */
     public int getGameNr(int x, int y){
         return game[x][y][0];
     }
+
+        /**
+         * sets a value at specified row and column
+         * @param x specified row
+         * @param y specified column
+         * @param value what number to set on position
+         */
     public void setGameNr(int x, int y, int value){
         game[x][y][0] = value;
         updateFacadeGameState();
     }
 
+        /**
+         * returns the value at specified row and columnat at games initial positions
+         * @param x specified row
+         * @param y specified column
+         * @return
+         */
     public int getGameAtStartNumber(int x, int y) {
         return gameAtStartCopy[x][y];
     }
 
+        /**
+         * Copies the (initial) game state to a separate array that keeps track
+         * of numbers at starting position.
+         * @return the array that was created
+         */
     int[][] makeGameAtStartCopy() {
         for (int i = 0; i < GRID_SIZE; i++) {
             for (int j = 0; j < GRID_SIZE; j++) {
@@ -164,6 +230,7 @@ import static se.kth.AlgotVREmilW.labb4.model.SudokuUtilities.*;
         }
     }
 
+   
     public void reverseVertical(){
         int[] tmpUnsolved = new int[9];
         int[] tmpSolved = new int[9];
