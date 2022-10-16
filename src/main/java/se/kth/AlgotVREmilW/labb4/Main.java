b79cce5a7bafd0be32a260c9eac7cd7c05f87604
AlTheMan <algot98@hotmail.com>
2022-10-16 22:31:55 +0200

    favicon (men behöver lägga bilderna rätt dock)

--- src/main/java/se/kth/AlgotVREmilW/labb4/Main.java
+++ src/main/java/se/kth/AlgotVREmilW/labb4/Main.java
@@ -4,11 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import se.kth.AlgotVREmilW.labb4.model.Facade;
 import se.kth.AlgotVREmilW.labb4.view.SudokuView;
 import static se.kth.AlgotVREmilW.labb4.model.SudokuUtilities.*;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main extends Application {
@@ -21,6 +23,11 @@ public class Main extends Application {
         Scene scene = new Scene(view);
         //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 
+        //lägger till favvicon: / application icon:
+        File application_icon_dir = new File(System.getProperty("user.home"), "/labb4_spara/favicon6.png");  //öppnar en specifik mapp (directory)
+        if (application_icon_dir.exists()){
+            stage.getIcons().add(new Image(String.valueOf(application_icon_dir)));
+        }
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.setResizable(false);  //TODO: kanske gör detta på ett annat sätt
